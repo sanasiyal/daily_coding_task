14649b506022a616384d734c3b75ea5066fb17e2
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2025-04-06 01:39:36 +0500

    Update task 2.py

--- task2/task 2.py
+++ task2/task 2.py
@@ -1,3 +1,12 @@
+ Mission :
+You are a robot assistant, and your job is to ask the user their name before starting a secret mission! 
+
+ Instructions :
+Ask the user for their name using input().
+Display a cool welcome message like a high-tech AI would!
+
+
+
 name = input("Enter your name: ")
 print(f"Processing... Name detected: {name}")
 print(f"Access Granted! Welcome, Agent {name}. Your mission awaits!")
