8de45cd12479b639f4fbb152744e2563b34da559
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2025-04-06 01:43:04 +0500

    Update task 4.py

--- task4/task 4.py
+++ task4/task 4.py
@@ -1,3 +1,12 @@
+ Mission:
+Create a simple program that checks whether a number is Even or Odd.
+
+ Instructions:
+ Ask the user to enter a number.
+ Check if the number is even or odd using the modulus (%) operator.
+ Display the result in a friendly message.
+
+
 user = int(input("Enter any number:"))
 
 if user % 2 == 0:
